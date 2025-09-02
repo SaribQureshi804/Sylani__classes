98d412d2bb83b28cf976c1724154e64746bfe554
SaribQureshi804 <saribqureshi60gmail.com>
2025-09-02 17:03:09 +0500

    assignmentno3

---
+++ assignment03/app.js
@@ -0,0 +1,51 @@
+//let fruit =["apple", "banana", "cherry", "date", "stawberry"];
+//console.log(fruit);
+
+//for (let i = 0; i < fruit.length; i++) {
+  //  console.log(fruit[i]);
+//}
+//let fruitt = fruit.splice(2, 2, "kiwi");
+///console.log(fruitt);
+
+//for ( let i= 0; i< fruit.length || fruit === fruitt;  i++){
+  //  console.log(fruit[i]);
+//}
+
+
+//let fruitadd = fruit.push("orange", "kiwi",);
+//let fruitsub = fruit.pop();
+//console.log(fruitadd);
+//console.log(fruitsub);
+
+// for (let i=0; i < fruit.length || fruit === fruitadd; i++ ){
+//    console.log(fruit[i]);
+//}
+
+// let numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// let found = false;
+// for (let i = 0; i < numbers.length; i++) {
+//        if ( numbers[i] > 50 )
+//          found = true;
+//         break;       
+//     }
+// console.log(found);
+
+//     if (found) {
+//   console.log("A number greater than 50 exists.");
+// } else {
+//   console.log("No number greater than 50 found.");
+// }
+
+
+let tables = prompt("Enter a number to generate its multiplication table:");
+let times = prompt("Enter the number of times to multiply:");
+
+
+ for (let i = tables; i <= times; i++) {
+    console.log(`${tables} x ${i} = ${tables * i}`);
+ }
+
+
+
+
+
