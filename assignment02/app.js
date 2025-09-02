af571e984cf81da71036f8922a3066384c3b1f14
SaribQureshi804 <saribqureshi60gmail.com>
2025-09-02 17:00:58 +0500

    assignmentno2

---
+++ assignment02/app.js
@@ -0,0 +1,49 @@
+// // let calculator = +prompt("Enter a number to calculation:");
+// // let calculator2 = +prompt("Enter another number to calculation:");
+// // let operator = prompt("Enter an operator (+, -, *, /,%):");
+
+// // if (operator === "+") {
+// //     alert(calculator++ + calculator2);
+// // }
+// // else if (operator === "-") {
+// //     alert(calculator - calculator2);
+// // }
+// // else if (operator === "*") {
+// //     alert(calculator * calculator2);
+// // }
+// // else if (operator === "/") {
+// //     alert(calculator / calculator2);
+// // }
+// // else if (operator === "%") {
+// //     alert(calculator % calculator2);
+// // console.log(calulator, calculator2, operator);
+// // }
+
+
+// let age = +prompt("Enter your age: ");
+// if (age >= 18) {
+//     alert("You are eligible to vote.");
+// }   else if (age < 18) {
+//     alert("You are not eligible to vote.");
+// }
+// else {
+//     alert("Invalid age entered.");
+// console.log
+// }
+
+
+let temprature = +prompt("Enter the temperature in Celsius: ");
+
+if (temprature < 10 ){
+    console.log("Too cold");
+}
+else if (temprature >= 10 && temprature < 25) {
+    console.log("Just right");
+}
+else if (temprature >= 25 ){
+    console.log("Too hot");
+}
+else if (temprature === 0) {
+    console.log("Freezing ");
+}
+
