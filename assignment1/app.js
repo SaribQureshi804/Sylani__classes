343ded4a3084f2bbabc61c877e5e570e586ad0d1
SaribQureshi804 <saribqureshi60gmail.com>
2025-09-02 12:51:21 +0500

    assignmentno1

---
+++ assignment1/app.js
@@ -0,0 +1,17 @@
+//alert ("Welcome to js Land...  \n Happy coding! " );
+
+//alert("Welcome to js land...");
+
+
+//alert("Happy Coding!");
+//alert("Happy Coding!");
+//alert("Happy Coding!");
+//alert("Happy Coding!");
+//alert("Happy Coding!");
+
+alert("Happy coding!");
+
+
+alert("Helo... I can run js through my web browser's console");
+
+
