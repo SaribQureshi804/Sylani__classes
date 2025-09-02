d8894e9e2b7ffb3daf642cf3fc66764e395f1f11
SaribQureshi804 <saribqureshi60gmail.com>
2025-09-02 17:06:58 +0500

    chapter5

---
+++ chapter5/app.js
@@ -0,0 +1,157 @@
+//let num1 =3;
+//let num2 =5;
+//let sum = num1 + num2;
+//console.log(" Sum of " + num1 + " and " +  num2  + " is " +  sum   );
+
+
+
+//let num1 =10;
+//let num2 =9;
+//let sub = num1 - num2;
+//console.log(" Sub of " + num1 + " and " +  num2  + " is " +  sub   );
+
+//let num1 =10;
+//let num2 =9;
+//let multiple = num1 * num2;
+//console.log(" multiple of " + num1 + " and " +  num2  + " is " +  multiple);
+
+
+//let num1 =2;
+//let num2 =12;
+//let divide = num1 % num2;
+//console.log(" divide of " + num1 + " and " +  num2  + " is " +  divide);
+
+
+//let num1 = 5;
+//let num2 = 7;
+//let num3 = 3;
+//console.log(num1++);
+//console.log(num1);
+//let sum = num1 + num2;
+//console.log( sum);
+//console.log(--sum);
+//let divide = sum % num3;
+//console.log(divide);
+
+//let ticket = 600;
+//let buy = 5;
+//let multiple = ticket * buy;
+//console.log("Total cost to buy " + buy + " ticket to a movie is " + multiple +"PKR" );
+
+
+
+
+//let userInput = prompt("Enter a number!");
+
+//console.log(userInput + " x " + 1 + " = " + userInput * 1);
+//console.log(userInput + " x " + 2 + " = " + userInput * 2);
+//console.log(userInput + " x " + 3 + " = " + userInput * 3);
+//console.log(userInput + " x " + 4 + " = " + userInput * 4);
+//console.log(userInput + " x " + 5 + " = " + userInput * 5);
+//console.log(userInput + " x " + 6 + " = " + userInput * 6);
+//console.log(userInput + " x " + 7 + " = " + userInput * 7);
+//console.log(userInput + " x " + 8 + " = " + userInput * 8);
+//console.log(userInput + " x " + 9 + " = " + userInput * 9);
+//console.log(userInput + " x " + 10 + " = " + userInput * 10);
+
+
+//let initialNumber = 0;
+//let userInput = +prompt("Enter a number!"); 
+//console.log(userInput + " x " + ++initialNumber + " = " + initialNumber * userInput );
+//console.log(userInput + " x " + ++initialNumber + " = " + initialNumber * userInput );
+//console.log(userInput + " x " + ++initialNumber + " = " + initialNumber * userInput );
+//console.log(userInput + " x " + ++initialNumber + " = " + initialNumber * userInput );
+//console.log(userInput + " x " + ++initialNumber + " = " + initialNumber * userInput );
+//console.log(userInput + " x " + ++initialNumber + " = " + initialNumber * userInput );
+//console.log(userInput + " x " + ++initialNumber + " = " + initialNumber * userInput );
+//console.log(userInput + " x " + ++initialNumber + " = " + initialNumber * userInput );
+//console.log(userInput + " x " + ++initialNumber + " = " + initialNumber * userInput );
+//console.log(userInput + " x " + ++initialNumber + " = " + initialNumber * userInput );
+
+
+//let celsius = prompt("Enter the celsius temprature");
+//let farenheit = (celsius * 9/5 ) + 32;
+//console.log(celsius +"C" + " is " + farenheit +"F");
+
+
+//let farenheit1 = prompt("Enter the farenheit temprature");
+//let celsius1 = (farenheit1 - 32 ) * 5/9;
+//console.log(farenheit1 +"F" + " is " + celsius1 +"C");
+
+//let item1 = 650;
+//console.log(item1);
+
+//let quantity = prompt("enter the quantity");
+//let multiple = (item1 * quantity);
+//console.log(item1 +" is " + quantity);
+
+//let item2 = 100;
+//console.log(item2);
+
+//let quantity1 = prompt("Enter a quantity");
+//let multiply =(item2 * quantity1);
+//console.log(item2 +" is " + quantity1);
+
+
+//let addition = (multiple + multiply);
+//console.log("Total cost of your order is " + addition);
+
+
+
+//let Totalmarks = 980;
+//console.log(Totalmarks);
+//let obtainedmarks = prompt("Enter a obtained marks");
+//console.log(obtainedmarks);
+//let percentage = (obtainedmarks/980) *100;
+//console.log("percentage: " + percentage);
+
+//let USDollar =285;
+//let PKR = prompt("Enter a PKR");
+//let multiple = (USDollar * PKR);
+//console.log(multiple);
+
+//let SaudiRiyal =85;
+//let pkr = prompt("entetr a pkr");
+//let multiply =(SaudiRiyal* pkr);
+//console.log(multiply);
+
+//let multiplies = (multiple + multiply);
+//console.log("Total currency in PKR: " + multiplies);
+
+
+ 
+//let arthimatic =( 9 + 8 *10 % 2);
+//console.log(arthimatic);
+
+//let h1 = document.getElementById("text");
+
+//let currentyear = 2025;
+//console.log( "Current year: " + currentyear)
+
+//let birthyear= 2006;
+//console.log("Birth year: " + birthyear);
+
+//let calculate   =(currentyear - birthyear);
+//console.log(calculate);
+
+//let radius =prompt("enter a radius of a circle value");
+//console.log("Radius of circle " + radius);
+//let pi =3.142;
+//let curcumference = ( 2 *pi * radius);
+//console.log("The curcumference of circle  " + curcumference);
+//let area = (pi * radius*radius);
+///console.log("Total area " + area);
+
+let snacks = "choclate Chip";
+console.log("favourite snacks: " + snacks);
+let currentyear = 15;
+console.log("Current age: " + currentyear);
+let estimatedyear = 66;
+console.log("Estimated  Maximum age: " + estimatedyear);
+let perday = 3;
+console.log("Amiunt of snacks per day");
+
+let calculation = (estimatedyear - currentyear * perday);
+console.log("You will need " + calculation + "choclate chip to last you until you ripe old age is " + estimatedyear);
+
+
