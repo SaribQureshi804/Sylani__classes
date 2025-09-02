36a5beaa4159a43d8298705792cf941d158c392f
SaribQureshi804 <saribqureshi60gmail.com>
2025-09-02 17:13:12 +0500

    firstlab

---
+++ assignment04/app.js
@@ -0,0 +1,6 @@
+let username = [prompt("Enter your name:")];
+
+let fullname = username.charAt(0).tolowerCase();
+
+console.log(fullname);
+
---
+++ first lab/app.js
@@ -0,0 +1,32 @@
+
+//let username = prompt("Enter your Name ") 
+
+//console.log("username")
+
+///let firstinput= +prompt("Enter your first number")
+//let secodinput= +prompt("Enter your Second number")
+
+
+//document.write(firrstinput + secondinput);
+
+
+//array [] Cars
+//let Cars = [ "Honda", "Toyota", "Buggatti", "Porsche","BMW" ];
+
+//let spckcar = car[4] = Ferrari;
+
+//let addcar = car[5] = Audio;
+
+//console.log(cars);
+    
+//let usrname= prompt("Enter your Username");
+ //let userhobby=prompt("enter your most favourite hobby") ;
+//let userhobby2=prompt("enter your secon
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
