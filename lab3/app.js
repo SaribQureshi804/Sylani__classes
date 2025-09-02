189d22a218caba45eea3a6d59275f328a6c01fa3
SaribQureshi804 <saribqureshi60gmail.com>
2025-09-02 17:16:07 +0500

    lab3

---
+++ lab3/app.js
@@ -0,0 +1,73 @@
+// // // // let age = prompt("enter your age");
+
+// // // // if (age >= 18) {
+// // // //     console.log("you are eligible for vote");
+// // // // }
+// // // // else if (age < 18 ){
+// // // //     console.log("you are not eligible for vote");
+
+// // // // }
+// // // // else {
+// // // //     console.log("invalid character");
+// // // // }
+
+// // // let age = prompt("Enter your age");
+// // // age = parseInt(age);
+
+// // // if (isNaN(age)){
+// // //  console.log("Invalid character");
+// // // }
+// // // else if (age >= 18 ){
+// // //      console.log("you are  eligible for vote");
+// // // }
+// // // else {
+// // //     console.log("you are not eligible for vote");
+// // // }
+
+// // let speed = "66Km/h";
+// // speed = parseInt(speed);
+
+
+// //  if( speed <=79){
+// //     console.log("no challan");
+// // }
+// // else {
+// //     console.log("Pay heavy fine");
+// // }
+
+
+// let salary =50000;
+// let bonus ="12.5%";
+// let bonusamount = paresFloat(bonus)
+// let bonusamount = parseFloat(bonus) / 100 * salary;
+// console.log( (bonusamount + salary) - 20000 );
+
+// let Discount ="70%";
+// let product = "23000TV";
+// let product1 = "20000mobile";
+// let product2 = "40000laptop";
+
+// let discountamount = parseFloat(Discount) / 100 * parseFloat(product);
+// console.log("")
+// let discountamount1 = parseFloat(Discount) / 100 * parseFloat(product1);
+// let c= parseFloat(Discount) / 100 * parseFloat(product2);
+
+// // consolelog(discountamount + discountamount1 + discountamount2 );
+// console.log(discountamount);
+// console.log(discountamount1);
+// console.log(c);
+// // console.log(discountamount + discountamount1 + c);
+// console.log("Total Discount Amount: " + (discountamount + discountamount1 + c));    
+
+
+
+
+
+let bill = prompt("Enter your bill");
+let people = prompt("Enter number of people");
+
+
+
+
+
+
